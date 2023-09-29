87f6db1156923853c9ae32b958480f62c8fa29c1
LorenzoFerFra <39970420+LorenzoFerFra@users.noreply.github.com>
2023-09-28 23:35:53 -0300

    agregado dom(todavia no anda)

--- app.js
+++ app.js
@@ -1,104 +1,125 @@
 class Producto{
-    constructor(id,nombre,precio){
-        this.id = id
-        this.nombre = nombre
-        this.precio = precio
-        this.cantidad = 0
-    }
-
-    agregarCantidad(cantidadDeseada){
-        this.cantidad = this.cantidad + cantidadDeseada
-    }
-
-    descripcion(){
-        return "id: "+this.id+ " nombre: "+this.nombre+ " precio: $"+this.precio+"\n"
+    constructor(id, nombre, precio, descripcion, img, alt){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.cantidad = 1;
+        this.img = img;
+        this.alt = alt;
+        
     }
 
     descripcionCarrito(){
-        return "id: "+this.id+ " nombre: "+this.nombre+ " precio: $"+this.precio+ " cantidad: "+this.cantidad+"\n"
+        return `
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${this.img}" class="img-fluid rounded-start" alt="${this.alt}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${this.nombre}</h5>
+                        <p class="card-text">Cantidad: ${this.cantidad}</p>
+                        <p class="card-text">Precio: ${this.precio}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }
+
+    descripcionProducto(){
+        return `
+        <div class="card" style="width: 32rem;">
+        <img src="${producto.img}" class="card-img-top" alt="${producto.alt}">
+        <div class="card-body">
+          <h2 class="card-title">${producto.nombre}</h2>
+          <p class="card-text">${producto.descripcion}</p>
+          <p class="card-text">$${producto.precio}</p>
+          <a href="#" class="btn btn-primary">Agregar al carrito</a>
+        </div>
+      </div>` 
     }
 }
 
 class Carrito{
-    constructor(){
+    constructor() {
         this.listaCarrito = []
     }
 
-    agregar(productoNuevo){
-        let existe = this.listaCarrito.some(producto => producto.id == productoNuevo.id)
-        if(!existe){
-            this.listaCarrito.push(productoNuevo)
+    agregar(producto) {
+        if( producto instanceof Producto){
+            this.listaCarrito.push(producto)
         }
     }
-
-    mostrar(){
-        let descripcionListaCompra = "Carrito: \n\n"
-        this.listaCarrito.forEach( producto => {
-            descripcionListaCompra = descripcionListaCompra + producto.descripcionCarrito()
+    mostrarProductos() {
+        let tiendaContainer = document.getElementById("tiendaContainer")
+        tiendaContainer.innerHTML = ""
+        this.listaCarrito.forEach(producto => {
+            tiendaContainer.innerHTML += producto.descripcionCarrito();
         })
-        return descripcionListaCompra
     }
-
-    calcularTotal(){
-        return this.listaCarrito.reduce( (total,producto) => total + producto.precio * producto.cantidad ,0)
-    }
+    // calcularTotal(){
+    //     return this.listaCarrito.reduce( (total,producto) => total + producto.precio * producto.cantidad ,0)
+    // }
 }
 
-class ProductoController{
+class ProductController{
     constructor(){
         this.listaProductos = []
     }
 
-    agregar(producto){
-        this.listaProductos.push(producto)
+    agregar(producto) {
+        if( producto instanceof Producto){
+            this.listaProductos.push(producto)
+        }
+    }
+    cargarProductos(){
+        this.agregar( new producto(1,"Peluche", 50000, "Muñeco de peluche fumo de koishi de touhou proyect", "../assets/img/peluche.webp", "koishi fumo"))
     }
 
-    mostrar(){
-        let descripcionListaProductos = "Recuerde el ID del producto que desea comprar\n\n"
-        this.listaProductos.forEach( producto => {
-            //descripcionListaProductos = descripcionListaProductos + " id: "+producto.id+ " nombre: "+producto.nombre+ " precio: $"+producto.precio+"\n"
-            descripcionListaProductos = descripcionListaProductos + producto.descripcion()
+    //mostrar productos en DOM
+    mostrarProductos(){
+        let tiendaContainer = document.getElementById("tiendaContainer")
+
+        this.listaProductos.forEach(producto => {
+            tiendaContainer.innerHTML += producto.descripcionProducto() 
         })
-        return descripcionListaProductos
-    }
+        
+        this.listaProductos.forEach(producto => {
+            const btn_ap = document.getElementById(`ap-${producto.id}`)
 
+            btn_ap.addEventListener("click",()=>{
+                carrito.agregar(producto)
+                carrito.mostrarProductos()
+            })
+        })
+        
+    }
     buscarId(id){
         return this.listaProductos.find(producto => producto.id == id)
     }
 }
 
-//creamos productos
-const p1 = new Producto(1,"Spinner", 300)
-const p2 = new Producto(2,"Kazoo", 600) 
-const p3 = new Producto(3,"Auriculares", 5000) 
-const p4 = new Producto(4,"Monitor", 40000) 
-const p5 = new Producto(5,"Joystick", 15000) 
+// //instanciar productos
+// const p1 = new Producto(1,"Peluche", 50000, "Muñeco de peluche fumo de koishi de touhou proyect", "../assets/img/peluche.webp", "koishi fumo") 
+// const p2 = new Producto(2,"Spinner", 600, "Muñeco de peluche fumo de koishi de touhou proyect", "img", "koishi fumo") 
+// const p3 = new Producto(3,"Joystick", 15000, "Muñeco de peluche fumo de koishi de touhou proyect", "../assets/img/peluche.webp", "koishi fumo") 
+// const p4 = new Producto(4,"Peluche", 40000, "Muñeco de peluche fumo de koishi de touhou proyect", "img", "koishi fumo") 
+// const p5 = new Producto(5,"Kazoo", 600, "Muñeco de peluche fumo de koishi de touhou proyect", "img", "koishi fumo") 
 
+// //mostrar los productos
+// controladorP.agregar(p1)
+// controladorP.agregar(p2)
+// controladorP.agregar(p3)
+// controladorP.agregar(p4)
+// controladorP.agregar(p5)
 
-
+const CP = new ProductController()
 const carrito = new Carrito()
-const controladorP = new ProductoController()
-
-controladorP.agregar(p1)
-controladorP.agregar(p2)
-controladorP.agregar(p3)
-controladorP.agregar(p4)
-controladorP.agregar(p5)
-
-let rta
-
-do{
-    alert( controladorP.mostrar() )
-    let id = Number(prompt("Ingrese el ID del producto que desea comprar!"))
-    const producto = controladorP.buscarId(id)
-    let cantidadDeseada = Number(prompt("Ingrese la cantidad que desea"))
-    producto.agregarCantidad(cantidadDeseada)
-    carrito.agregar(producto)
-    alert( carrito.mostrar() )
 
 
-    rta = prompt("¿Desea finalizar la compra? (escriba 'SI' para finalizar)").toLowerCase()
-}while(rta != "si")
+CP.cargarProductos()
+CP.mostrarProductos()
 
-//mostrar total
-alert( "El total es de: $"+carrito.calcularTotal() )
+
