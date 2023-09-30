4aaf5fff1242ccb439ff217b1553202b151be4f6
LorenzoFerFra <39970420+LorenzoFerFra@users.noreply.github.com>
2023-09-29 23:23:35 -0300

    Local storage terminado
    Putas listascarrito, tienen todas las variables el mismo nombre para confundir y que piense?

--- app.js
+++ app.js
@@ -19,9 +19,12 @@ class Producto{
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">${this.nombre}</h5>
+                        <h3 class="card-title">${this.nombre}</h3>
                         <p class="card-text">Cantidad: ${this.cantidad}</p>
                         <p class="card-text">Precio: ${this.precio}</p>
+                        <button class="btn borrar_Btn" id="borrar_compra-${this.id}">
+                            <i class="fa-regular fa-trash-can"></i>
+                    </button>
                     </div>
                 </div>
             </div>
@@ -32,7 +35,7 @@ class Producto{
         return `
         <div class="card" style="width: 32rem;">
         <img src="${this.img}" class="card-img-top" alt="${this.alt}">
-        <div class="card-body">
+        <div class="card-body producto_bg">
           <h2 class="card-title">${this.nombre}</h2>
           <p class="card-text">${this.descripcion}</p>
           <p class="card-text">$${this.precio}</p>
@@ -52,38 +55,57 @@ class Carrito{
             this.listaCarrito.push(producto)
         }
     }
-    mostrarProductos() {
+    mostrarCarrito() {
         let contenedor_carrito = document.getElementById("contenedor_carrito")
         contenedor_carrito.innerHTML = ""
         this.listaCarrito.forEach(producto => {
             contenedor_carrito.innerHTML += producto.descripcionCarrito();
         })
     }
+        //Convertir a json y guardar el carrito en storage
+    carritoStorage(){
+        let listaCarritoJSON = JSON.stringify(this.listaCarrito)
+        localStorage.setItem("carritoJSON", listaCarritoJSON)
+    }
+        //Volver a convertir los JSON a objetos
+    carritoCargar(){
+        let listaCarritoJSON = localStorage.getItem("carritoJSON")
+        let listaCarritoJS = JSON.parse(listaCarritoJSON)
+        let listaTemp = []
+        listaCarritoJS.forEach( producto => {
+            let nuevoProducto = new Producto(producto.id, producto.nombre, producto.precio, producto.descripcion, producto.img, producto.alt)
+            listaTemp.push(nuevoProducto)
+        })
+        this.listaCarrito = listaTemp
+    }
+        
+    }
+ 
     // calcularTotal(){
     //     return this.listaCarrito.reduce( (total,producto) => total + producto.precio * producto.cantidad ,0)
     // }
-}
+
 
 class ProductController{
     constructor(){
         this.listaProductos = []
     }
-
+    //agregar produdcto nuevo al dom
     agregar(producto) {
         if( producto instanceof Producto){
             this.listaProductos.push(producto)
         }
     }
+    //cargar productos al dom
     cargarProductos(){
-        this.agregar( new Producto(1,"Peluche", 50000, "Muñeco de peluche fumo de koishi de touhou proyect", "../assets/img/peluche.webp", "koishi fumo") )
-        this.agregar(new Producto(2,"Spinner", 600, "Muñeco de peluche fumo de koishi de touhou proyect", "../assets/img/spinner.webp", "koishi fumo") )
-        this.agregar(new Producto(3, "ryzen 7", 250, "Un microprocesador de gama alta", "https://m.media-amazon.com/images/I/51D3DrDmwkL.__AC_SX300_SY300_QL70_ML2_.jpg", "un microprocesador amd") )
+        this.agregar( new Producto(1,"fumo", 50000, "Muñeco de peluche fumo de koishi de touhou proyect", "../assets/img/peluche.webp", "koishi fumo") )
+        this.agregar(new Producto(2,"Spinner", 600, "Un spinner dorado ", "../assets/img/spinner.webp", "spinner dorado") )
+        this.agregar(new Producto(3, "ryzen 7", 250, " gama alta", "https://m.media-amazon.com/images/I/51D3DrDmwkL.__AC_SX300_SY300_QL70_ML2_.jpg", "un microprocesador amd") )
      }
 
     //mostrar productos en DOM
     mostrarProductos(){
         let tiendaContainer = document.getElementById("tiendaContainer")
-
         this.listaProductos.forEach(producto => {
             tiendaContainer.innerHTML += producto.descripcionProducto() 
         })
@@ -93,7 +115,8 @@ class ProductController{
 
             btn_ap.addEventListener("click",()=>{
                 carrito.agregar(producto)
-                carrito.mostrarProductos()
+                carrito.carritoStorage()
+                carrito.mostrarCarrito()
             })
         })
         
@@ -103,9 +126,32 @@ class ProductController{
     }
 }
 
+
+
+// let carritoStorage = [
+//      new Producto(1,"fumo", 50000, "Muñeco de peluche fumo de koishi de touhou proyect", "../assets/img/peluche.webp", "koishi fumo"),
+//      new Producto(2,"Spinner", 600, "Un spinner dorado ", "../assets/img/spinner.webp", "spinner dorado") ,
+//      new Producto(3,"Sasdasdas", 600, "Un spinner dorado ", "../assets/img/spinner.webp", "spinner dorado") ,
+// ]
+
+// let carritoStorageJSON = JSON.stringify(carritoStorage)
+// localStorage.setItem("carritoJSON", carritoStorageJSON)
+
+// let listaCarritoJSON = localStorage.getItem("carritoJSON")
+// let listaCarritoJS = JSON.parse(listaCarritoJSON)
+// let listaTemp = []
+// listaCarritoJS.forEach( producto => {
+//     let nuevoProducto = new Producto(producto.id, producto.nombre, producto.precio, producto.descripcion, producto.img, producto.alt)
+//     listaTemp.push(nuevoProducto)
+// })
+// carritoJSON = listaTemp
+// console.log(carritoJSON)
+
 const CP = new ProductController()
 const carrito = new Carrito()
 
+carrito.carritoCargar()
+carrito.mostrarCarrito()
 
 CP.cargarProductos()
 CP.mostrarProductos()
